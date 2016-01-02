152641c6d231b32ef6ec9f18bf27b3773cfa7d6c
Isaac Hess <isaachess@gmail.com>
2016-01-02 09:18:44 -0700

    Working, reading from stdin and printing to stdout

--- matrix-orthogonal-distance.js
+++ matrix-orthogonal-distance.js
@@ -1,13 +1,70 @@
-// 1 2 3 4 5
-// 6 7 1 9 10
+// Given a matrix of numbers, and an integer k, determine whether any duplicate numbers lie within k orthogonal distance of each other.
 //
-// [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+// Reads from stdin. Give inputs in the following order:
+//
+// 1. How many rows to expect in the matrix (integer).
+// 2. Then give each row of the matrix, one line at a time (each value must be an integer).
+// 3. Specify k (integer).
+// 4. After the program evalutes, you may repeat the steps as many times as you wish.
+//
+// Example
+//
+// 1 2 3 4 5
+// 6 7 8 9 10
+// 3 4 1 23 15
+//
+// 3, 4, and 1 are duplicates in this matrix. All are 4 orthogonal distance away.
 
-var matrix = [[1, 2, 3, 4, 5], [6, 7, 1, 9, 10]]
-var k = 2
+var readline = require('readline');
 
-dupes = testOrthogonalDuplicates(matrix, k)
-console.log('dupes', dupes)
+var MATRIX_LINES, MATRIX_LINE_LENGTH, MATRIX, GLOBAL_K;
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+resetInputs();
+rl.on('line', function(line) {
+    if (!MATRIX_LINES) return storeMatrixLines(line);
+    if (MATRIX.length < MATRIX_LINES) return addMatrixLine(line);
+    if (!GLOBAL_K) return addKAndStart(line);
+    throw new Error('Cannot interpret input.');
+});
+
+function resetInputs() {
+    MATRIX_LINES = null;
+    MATRIX_LINE_LENGTH = null
+    MATRIX = [];
+    GLOBAL_K = null;
+}
+
+function storeMatrixLines(line) {
+    var lines = Number(line);
+    if (isNaN(lines)) throw new Error('First input must be integer of how many lines to expect for matrix.');
+    MATRIX_LINES = lines;
+}
+
+function addMatrixLine(line) {
+    var matrixLine = line.split(' ')
+    matrixLine = matrixLine.map(function(str) {
+        var num = Number(str);
+        if (isNaN(num)) throw new Error('Matrix lines must only consist of numbers.');
+        return num;
+    });
+    if (!MATRIX_LINE_LENGTH) MATRIX_LINE_LENGTH = matrixLine.length;
+    if (matrixLine.length != MATRIX_LINE_LENGTH) throw new Error('Each line in the matrix must be of the same length.');
+    MATRIX.push(matrixLine);
+}
+
+function addKAndStart(line) {
+    var k = Number(line);
+    if (isNaN(k)) throw new Error('k must be integer');
+    GLOBAL_K = k;
+    var dupes = testOrthogonalDuplicates(MATRIX, GLOBAL_K);
+    console.log(dupes)
+    resetInputs()
+}
 
 function testOrthogonalDuplicates(matrix, k) {
     if (k < 1) return false;
